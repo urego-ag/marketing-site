415a2f3e28c2d92d027909de79db3c604fc5fa51
Joel Chan <2838605+sinn1@users.noreply.github.com>
2021-07-19 17:20:53 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,52 +3,22 @@ import { Helmet } from 'react-helmet';
 import '../styles/index.css';
 
 function Index() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
       <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
+        <title>UREGO</title>
       </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/gatsby"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <div class="bgimg">
+        <div class="topcentre">
+          <img alt="Urego logo" src="images/UREGO_logo_white_claim_RGB.svg" style="width:50%;margin-left:auto;margin-right:auto;display:block;" />
+        </div>
+        <div class="middle">
+          <h1>COMING SOON</h1>
+        </div>
+        <div class="bottomleft">
+          <p>&copy;2021 Urego AG</p>
+        </div>
+      </div>
     </main>
   );
 }
